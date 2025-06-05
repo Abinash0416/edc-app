fc562c73a4efab60a15ec45eceeb06e497c5cf64
Abinash0416 <abiabinash0416@gmail.com>
2025-06-05 12:39:36 +0530

    added docker-compose

--- connector/app/build.gradle.kts
+++ connector/app/build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
 
 
 
-    implementation(libs.edc.control.api.configuration)
     implementation(libs.edc.control.plane.api.client)
     implementation(libs.edc.control.plane.api)
     implementation(libs.edc.control.plane.core)
@@ -27,14 +26,11 @@ dependencies {
     implementation(libs.edc.management.api)
     implementation(libs.edc.transfer.data.plane.signaling)
     implementation(libs.edc.validator.data.address.http.data)
-
     implementation(libs.edc.edr.cache.api)
     implementation(libs.edc.edr.store.core)
     implementation(libs.edc.edr.store.receiver)
-
     implementation(libs.edc.data.plane.selector.api)
     implementation(libs.edc.data.plane.selector.core)
-
     implementation(libs.edc.data.plane.self.registration)
     implementation(libs.edc.data.plane.signaling.api)
     implementation(libs.edc.data.plane.core)
